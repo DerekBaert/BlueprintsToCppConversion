a50038990d5a5035e4379789bb9db286f0616c73
Derek Baert~ <derek.baert@gmail.com>
2023-03-13 20:26:08 -0500

    Completed Video 11

--- Source/BlueprintsToCpp/Quests/QuestManager.h
+++ Source/BlueprintsToCpp/Quests/QuestManager.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "QuestInfo.h"
 #include "QuestManager.generated.h"
+
 
 UCLASS()
 class BLUEPRINTSTOCPP_API AQuestManager : public AActor
@@ -18,9 +20,13 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FQuestInfo> QuestList;
 };
